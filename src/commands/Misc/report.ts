1a6c501594dde8127fa1a87ef7e8c98b6a32bc14
Sobhan Rasoulzadeh Asl <sobhan.rasoulzadeh.asl@gmail.com>
2026-02-25 17:14:30 +0330

    Update report.ts

--- src/commands/Misc/report.ts
+++ src/commands/Misc/report.ts
@@ -3,12 +3,12 @@ import {
   ApplicationCommandType,
   ButtonBuilder,
   ButtonStyle,
+  LabelBuilder,
   ModalBuilder,
   PermissionsBitField,
   TextInputBuilder,
   TextInputStyle
 } from "discord.js";
-import { isBaseInteraction } from "../../utils/interactionTools";
 import { CommandType } from "../../types/interfaces";
 import selectLanguage from "../../utils/selectLanguage";
 import EmbedData from "../../storage/EmbedData";
@@ -34,51 +34,30 @@ export default {
     dm_permission: true
   },
   category: "misc",
-  aliases: ["h", "commands"],
   cooldown: 10,
-  only_owner: false,
-  only_slash: true,
-  only_message: true,
 
-  run: async (client, interaction, args) => {
+  run: async (client, interaction) => {
     try {
       const guildId = interaction.guildId!;
       const lang = (await dbAccess.getLanguage(guildId)) || config.discord.default_language;
       const language = selectLanguage(lang);
 
-      if (isBaseInteraction(interaction)) {
-        const modal = new ModalBuilder()
-          .setTitle(language.replies.modals.reportModalTitle)
-          .setCustomId("reportModal")
-          .addComponents(
-            new ActionRowBuilder<TextInputBuilder>()
-              .addComponents(
-                new TextInputBuilder()
-                  .setCustomId("reportModalMessage")
-                  .setLabel(language.replies.modals.reportModalLabel)
-                  .setPlaceholder(language.replies.modals.reportModalPlaceholder)
-                  .setStyle(TextInputStyle.Paragraph)
-              )
-          );
+      const modal = new ModalBuilder()
+        .setTitle(language.replies.modals.reportModalTitle)
+        .setCustomId("reportModal");
 
-        return await interaction.showModal(modal);
-      }
+      const modalLabel = new LabelBuilder()
+        .setLabel(language.replies.modals.reportModalLabel)
+        .setTextInputComponent(
+          new TextInputBuilder()
+            .setCustomId("reportModalMessage")
+            .setPlaceholder(language.replies.modals.reportModalPlaceholder)
+            .setStyle(TextInputStyle.Paragraph)
+        )
 
-      else {
-        return await response(interaction, {
-          content: " ",
-          components: [
-            new ActionRowBuilder<ButtonBuilder>()
-              .addComponents(
-                new ButtonBuilder()
-                  .setStyle(ButtonStyle.Secondary)
-                  .setEmoji(EmbedData.emotes.default.report)
-                  .setLabel(language.commands.report.replies.reportButton)
-                  .setCustomId("reportButton")
-              )
-          ]
-        })
-      }
+      modal.addLabelComponents(modalLabel)
+
+      return await interaction.showModal(modal);
     }
 
     catch (e) {
