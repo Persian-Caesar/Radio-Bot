dbd2269e1f5341668adf4ff84e4b490c580507d8
Sobhan Rasoulzadeh Asl <sobhan.rasoulzadeh.asl@gmail.com>
2026-02-27 15:39:59 +0330

    Update EmbedData.ts

--- src/storage/EmbedData.ts
+++ src/storage/EmbedData.ts
@@ -1,8 +1,8 @@
 export default {
   update: "__New bot's version is here!__\n### Version 0.0.2 - Initial Release 🛠️\n- **Basic bot setup** with essential commands.\n- Integrated **voice channel functionality** for playing radio.\n- Added **AFK management** and **status updates**.\n- Configurable **.env file** for easy setup.\n- Initial **database options**: JSON, MySQL, MongoDB.",
   footer: {
-    footerIcon: "https://cdn.discordapp.com/avatars/865630940361785345/d0c85fbbdb0ee9f105336a041904e7d8.png?size=4096",
-    footerText: "©️ Persian Caesar"
+    footerIcon: "https://i.ibb.co/8L3HJsTQ/3-modified.png",
+    footerText: "© Copyright by Persian Caesar"
   },
   color: {
     theme: "#f11461",
